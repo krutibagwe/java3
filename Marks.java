38fe662914103dfece8054f8346e28a6175fd0d0
krutibagwe <83567614+krutibagwe@users.noreply.github.com>
2023-10-31 01:25:04 +0530

    Create Marks.java

---
+++ Marks.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class MarksOutOfBoundException extends Exception{
+    public MarksOutOfBoundException(String message){
+        super(message);
+    }
+}
+public class Marks{
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the marks: ");
+        int mrks= scanner.nextInt();
+        
+        try{
+            if(mrks<0 || mrks>100){
+                throw new MarksOutOfBoundException("Marks should be between 0 and 100");
+        }
+        System.out.println("Entered marks are valid: " + mrks);
+    } catch (MarksOutOfBoundException e){
+        System.out.println("Exception: "+ e.getMessage());
+        
+    } finally {
+        scanner.close();
+    }
+}
+}
