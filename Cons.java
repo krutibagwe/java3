6ed80b6d9502f420d260d0a426baa0e80355994c
krutibagwe <83567614+krutibagwe@users.noreply.github.com>
2023-10-31 01:23:40 +0530

    Create Cons.java

---
+++ Cons.java
@@ -0,0 +1,19 @@
+class Person{
+    private String name;
+    
+    public Person(String name){
+        this.name=name;
+    }
+    public void printInfo(){
+        System.out.println("Hello " + this.name);
+    }
+}
+public class Cons{
+    public static void main(String [] args){
+        Person p1= new Person("Alice");
+        Person p2= new Person("Bob");
+        
+        p1.printInfo();
+        p2.printInfo();
+    }
+}
