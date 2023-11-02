99996d4af6e8facb275c69c395fcb37164fa7175
krutibagwe <83567614+krutibagwe@users.noreply.github.com>
2023-11-03 03:33:51 +0530

    Update Week.java

--- Week.java
+++ Week.java
@@ -1,7 +1,7 @@
 public class Week{
     public static void main(String[]args){
         Scanner scanner= new Scanner(System.in);
-        System.out.println("Enter \n 1. for Monday \n 2. for Tuesday");
+        System.out.println("Enter \n 1. for Monday \n 2. for Tuesday\n 3. for Wednesday\n");
         System.out.println("Enter your choice: ");
         int choice = scanner.nextInt();
         
@@ -11,6 +11,9 @@ public class Week{
                 break;
             case 2: 
                 System.out.println("Tuesday");
+                break;
+             case 3: 
+                System.out.println("Wednesday");
                 break;
             default:
             System.out.println("Wrong choice.");
